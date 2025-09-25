23d7c8f97d1a663179833551241cf5cedfbcc8ea
Ebba Lundgren <ebba@MacBook-Air-som-tillhor-Ebba.local>
2025-09-25 10:00:26 +0200

    trying again

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+// src/pages/api/auth/[...nextauth].ts
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -6,7 +7,16 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      // allowDangerousEmailAccountLinking: true // bara vid behov
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  pages: { signIn: "/login" },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith("/")) return baseUrl + url;
+      if (new URL(url).origin === baseUrl) return url;
+      return baseUrl + "/main";
+    },
+  },
 });
