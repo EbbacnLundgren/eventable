b7aa5ae1f096cc88acff65ca417ba81fb86ae5e5
Ebba Lundgren <ebba@MacBook-Air-som-tillhor-Ebba.local>
2025-10-09 10:09:42 +0200

    Did some sort of design on frontpage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
-    "./src/pages/**/*.{ts,tsx}" // om du har pages-folder
+    './src/app/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+    './src/pages/**/*.{ts,tsx}', // om du har pages-folder
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        baloo: ['"Baloo 2"', 'cursive'],
+      },
+
+      colors: {
+        champagne: '#fdf3e7',
+      },
+    },
   },
   plugins: [],
-};
+}
