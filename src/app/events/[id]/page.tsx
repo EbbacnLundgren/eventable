f17a1aee704ae349e38910f5f659edb9ea2cd57d
Ebba Lundgren <ebba@MacBook-Air-som-tillhor-Ebba.local>
2025-10-16 11:02:45 +0200

    Trying to fix with all the client files

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { supabase } from '@/lib/client'
 import Image from 'next/image'
 import Link from 'next/link'
-// @ts-expect-error: Lucide Music icon works at runtime, this removes the red squiggly line
 import { Camera, Music } from 'lucide-react'
 import { ArrowLeft } from 'lucide-react'
 
