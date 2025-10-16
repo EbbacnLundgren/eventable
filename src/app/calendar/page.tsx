d488272ba95e91addd7c1b857acbea3b777b2c81
Alicia-Lundberg <alicia_lundberg@github.com>
2025-10-16 17:50:03 +0200

    prettier

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,15 +1,13 @@
-
 import CalendarComponent from '@/components/calendar'
 import Sidebar from '@/components/Sidebar'
 
 export default function Page() {
-    return (
-        <div className="flex min-h-screen">
-            <Sidebar />
-            <div className="flex flex-col flex-1 ml-20">
-                <CalendarComponent />
-
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex flex-col flex-1 ml-20">
+        <CalendarComponent />
+      </div>
+    </div>
+  )
 }
--- src/app/my-events/page.tsx
+++ src/app/my-events/page.tsx
@@ -1,8 +1,8 @@
 export default function Page() {
-    return (
-        <div>
-            <h1>TODO</h1>
-            <p>Denna sida är under konstruktion.</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>TODO</h1>
+      <p>Denna sida är under konstruktion.</p>
+    </div>
+  )
 }
