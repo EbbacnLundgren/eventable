26a5c9eea0973c027e58240b1d2369da79987f0e
Ebba Lundgren <ebba@MacBook-Air-som-tillhor-Ebba.local>
2025-10-16 08:47:17 +0200

    Added login in button... and fixed issues with favicon

--- src/components/loginBox.tsx
+++ src/components/loginBox.tsx
@@ -74,7 +74,15 @@ export default function LoginBox() {
           </button>
         </div>
 
-        <div className="text-center mt-1">
+        <div className="text-center mt-1 flex flex-col items-center gap-2">
+          <button
+            type="submit"
+            onClick={() => router.push('/main')}
+            className="p-2 mt-2 rounded text-white bg-gradient-to-r from-pink-500 to-pink-600 hover:from-pink-600 hover:to-pink-700 transition font-semibold"
+          >
+            Log in
+          </button>
+
           <Link
             href="/reset-password"
             className="text-sm text-pink-600 hover:text-pink-700 underline center"
