8337217499096eec5d4611c9fafe299c3f8eecb0
Ebba Lundgren <ebbacn.lundgren@gmail.com>
2025-10-29 17:04:17 +0100

    prettier

--- src/components/eventsection.tsx
+++ src/components/eventsection.tsx
@@ -86,7 +86,7 @@ const EventsSection = ({
       matchesFilter =
         matchesFilter &&
         new Date(event.date).toISOString().split('T')[0] ===
-        advancedFilters.dateFrom
+          advancedFilters.dateFrom
     }
 
     if (advancedFilters.host) {
