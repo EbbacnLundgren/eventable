b61fef1605110eecf3ea6a66b158273b4786652d
Signe Johansson <signe.johansson33@gmail.com>
2025-10-30 13:00:09 +0100

    prettier

--- src/components/eventsection.tsx
+++ src/components/eventsection.tsx
@@ -84,7 +84,7 @@ const EventsSection = ({
       matchesFilter =
         matchesFilter &&
         new Date(event.date).toISOString().split('T')[0] ===
-        advancedFilters.dateFrom
+          advancedFilters.dateFrom
     }
 
     if (advancedFilters.host) {
@@ -149,10 +149,11 @@ const EventsSection = ({
 
                     {diffDays >= 0 && (
                       <span
-                        className={`ml-2 px-3 py-1 rounded-full text-sm font-semibold shadow-sm ${diffDays <= 7
+                        className={`ml-2 px-3 py-1 rounded-full text-sm font-semibold shadow-sm ${
+                          diffDays <= 7
                             ? 'bg-gradient-to-r from-pink-500 to-orange-400 text-white'
                             : 'bg-white/40 text-stone-800'
-                          }`}
+                        }`}
                       >
                         {diffDays === 0
                           ? 'Today'
