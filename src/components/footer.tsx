aba2595240c604726621f6e50c52d391a12af093
noraolofsson <nora.olofsson@hotmail.com>
2025-11-27 16:02:57 +0100

    Footer (prel design)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,27 @@
 export default function Footer() {
   return (
-    <footer className="w-full p-4 text-center text-gray-500 bg-gray-100">
-      &copy; 2025 Eventable
+    <footer className="w-full bg-white py-16 px-6 border-t border-gray-300">
+      <div className="max-w-5xl mx-auto text-center space-y-6 text-gray-600">
+        <h2 className="text-lg font-semibold text-gray-700">
+          We Are Eventable!
+        </h2>
+
+        <p className="text-sm">
+          A seamless way to create, find and explore events. Enjoy!
+        </p>
+
+        <p className="text-sm">
+          Contact us:{' '}
+          <a
+            href="mailto:contact@eventable.com"
+            className="text-blue-600 underline"
+          >
+            contacteventable@gmail.com
+          </a>
+        </p>
+
+        <p className="text-xs text-gray-500 mt-4">© 2025 Eventable</p>
+      </div>
     </footer>
   )
 }
