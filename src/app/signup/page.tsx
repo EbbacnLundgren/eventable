1a5dafec461c8edf777b33fd5d85e7dff923f92b
noraolofsson <nora.olofsson@hotmail.com>
2025-12-04 10:53:38 +0100

    back button is removed from login, signup and forgot password

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,15 +1,16 @@
 import SignupBox from '@/components/signUpBox'
-import Link from 'next/link'
+//import Link from 'next/link'
 
 export default function SignupPage() {
   return (
     <main className="min-h-screen flex items-center justify-center relative">
+      {/*
       <Link
         href="/"
         className="absolute left-4 top-4 px-3 py-1.5 rounded border border-black/20 bg-white/80 text-black text-sm hover:bg-white"
       >
         ← Back
-      </Link>
+      </Link>*/}
       <div className="min-h-screen w-full flex items-center justify-center bg-pink-100">
         <SignupBox />
       </div>
